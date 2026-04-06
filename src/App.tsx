fd165fb9df6aa4d67221b70d0c06c8e651637756
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2026-04-06 16:41:56 +0300

    temp: rename folders to fix case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.scss";
 import { Routes, Route, Link } from "react-router-dom";
-import { HomePage } from "./pages/home/Home";
-import { VariablesPage } from "./pages/variables/Variables";
-import { VariableDetailsPage } from "./pages/details";
+import { HomePage } from "./pages/home_temp/Home";
+import { VariablesPage } from "./pages/variables_temp/Variables";
+import { VariableDetailsPage } from "./pages/details_temp";
 
 function App() {
   return (
